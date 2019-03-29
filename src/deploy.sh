e1ea807d3db998b303ce7130f62275f397671f47
Philipp Arndt <2f.mail@gmx.de>
2019-03-29 12:04:23 +0100

    fixed deploy folder

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mvn clean install assembly:single
 if [ $? -eq 0 ]; then
-    scp ./de.rnd7.ws/target/deconz-to-mqtt-gw.jar 192.168.3.104:/home/pi/deconz-to-mqtt-gw.jar
+    scp ./de.rnd7.deconzmqttgw/target/deconz-to-mqtt-gw.jar 192.168.3.104:/home/pi/deconz-to-mqtt-gw.jar
     echo "deploy finished"
 else
     echo FAIL
